c4c4be4e15e4958053ed72ce6ac9aca6aa069d4c
bobjackie <jackie.aug31@gmail.com>
2025-11-12 20:13:23 +0000

    Comparison Operators in Python

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -41,3 +41,15 @@ print(10 == 9)
 
 # practice problem :
 # where a student must check if their score is greater than or equal to 60 to pass a test.# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
+
+score = int(input("Enter your score"))
+if score >= 60:
+    print("You passed the test")
+else:
+    print("You failed")
+
+password = input("Enter your password:")
+if len(password)>= 8 and any(char.isdigit() for char in password):
+    print ("Password is Valid")
+else:
+    print ("Wrong password")
